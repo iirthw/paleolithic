dc17967bdcf08debd95be2f8c7d4c291d6e99c8d
iirthw <iirthw@gmail.com>
2021-05-18 21:23:23 +0300

    start implementing test_bitmanip

--- test/test_bitmanip.cc
+++ test/test_bitmanip.cc
@@ -1,1 +1,21 @@
 #include "bitmanip/bitmanip.h"
+
+#include "gtest/gtest.h"
+
+class TestBitmanip : public ::testing::Test
+{
+    protected:
+        // Uncomment any or all of the following is their
+        // bodies would not be empty
+
+        // TestBitmanip() {}
+        // ~TestBitmanip() {}
+
+        // void SetUp() override {}
+        // void TearDown() override {}
+};
+
+TEST_F(TestBitmanip, SimpleIsPower2)
+{
+    ASSERT_TRUE(bitmanip::isPower2(2));
+}

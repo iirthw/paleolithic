ce64134c0ebe604332e06ad9617465fdf0269c07
iirthw <iirthw@gmail.com>
2021-08-22 22:49:18 +0300

    [object_pool] ctor with max capacity

--- lib/public/memory/object_pool.h
+++ lib/public/memory/object_pool.h
@@ -5,6 +5,12 @@ namespace memory
 	template <typename T>
 	class ObjectPool
 	{
+	public:
 
+		ObjectPool(std::size_t maxCapacity) : mMaxCapacity(maxCapacity) {}
+		
+	private:
+
+		std::size_t mMaxCapacity;
 	}; // class ObjectPool
 } // namespace memory

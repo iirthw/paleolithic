d9e4992e62812d3a7441c7f21cca1f7f39108ae8
iirthw <iirthw@gmail.com>
2021-08-23 23:30:34 +0300

    [object_pool] ctor protoptypes

--- lib/public/memory/object_pool.h
+++ lib/public/memory/object_pool.h
@@ -2,15 +2,27 @@
 
 namespace memory 
 {
+	/**
+	 * @brief Object pool data structure;
+	 * 
+	 * The pool can contain 1000+ objects, a user can specify desired number of 
+	 * objects in the ctor, it can't be made less than 100 though.
+	 */
 	template <typename T>
 	class ObjectPool
 	{
 	public:
 
-		ObjectPool(std::size_t capacity) : mCapacity(capacity) {}
+		// ---------------------------------------------------------------------
+		// ctors
+		ObejctPool() : mCapacity(min_capacity) {}
+		ObjectPool(std::size_t capacity) 
+		: mCapacity(capacity > min_capacity ? capacity : min_capacity) {}
 		
 	private:
 
 		std::size_t mCapacity;
+
+		static size_t min_capacity = 1000;
 	}; // class ObjectPool
 } // namespace memory

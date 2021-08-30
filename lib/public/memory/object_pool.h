5871a06b76518786ec1e4b3ac41e54329773885f
iirthw <iirthw@gmail.com>
2021-08-30 23:02:14 +0300

    [object_pool] wip on Chunk

--- lib/public/memory/object_pool.h
+++ lib/public/memory/object_pool.h
@@ -23,23 +23,47 @@ namespace memory
     } // log2ceil
 
     /**
-     * Chunk - a simple POD representation of memory blocks in the pool.
-     * Each object of type Chunk contains and manages a chunk of memory
-     * consisting of an integral number of fixed-size blocks. Chunk contains
-     * logic that allows to allocate and deallocate memory blocks.
-     * 
-     * When there are no more blocks available in the chunk, the allocation
-     * function fails by returning zero.
+     * @biref FixedAllocator aggregates an array of Chunks.
+     * Whenever a request for memory comes and all existing Chunks are occupied,
+     * FixedAllocator creates a new Chunk and appends it to the array. Then it
+     * forwards the request to the new Chunk.
      */
-    struct Chunk {
-    	void init(std::size_t blockSize,
-    			  unsigned char blocks);
-    	void* alloc(std::size_t blockSize);
-    	void dealloc(void* p, std::size blockSize);
-    	unsigned char* data_;
-    	unsigned char firstAvailableBlock_,
-    		blocksAvailable_;
+    class FixedAllocator {
+    private:
+		/**
+     	* Chunk - a simple POD representation of memory blocks in the pool.
+     	* Each object of type Chunk contains and manages a chunk of memory
+     	* consisting of an integral number of fixed-size blocks. Chunk contains
+     	* logic that allows to allocate and deallocate memory blocks.
+     	* 
+     	* When there are no more blocks available in the chunk, the allocation
+     	* function fails by returning zero.
+     	*/
+    	struct Chunk {
+	    	void init(std::size_t blockSize,
+	    			  unsigned char blocks);
+	    	void* alloc(std::size_t blockSize);
+	    	void dealloc(void* p, std::size blockSize);
+	    	unsigned char* data_;
+	    	unsigned char firstAvailableBlock_,
+	    		blocksAvailable_;
+    	};
     };
+
+
+    /**
+     * @brief SmallObjectAllocator provides general allocation and deallocation
+     * functions. It holds several FixedAllocator objects, each specialized for 
+     * allocating objects of one size. Depending on the number of bytes requsted,
+     * it dispatches memory allocation requests to one of its FixedAllocators
+     * or to the default operator new if the size requested is too large.
+     */
+
+    /**
+     * @brief SmallObject offers encapsulated allocation services for 
+     * c++ classes. Client objects benefit from specialized allocation by
+     * simply deriving from SmallObject.
+     */
 
 	/**
 	 * @brief Object pool data structure;

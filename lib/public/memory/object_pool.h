79ba49368979851007d06785a969fb8e706800be
iirthw <iirthw@gmail.com>
2021-08-31 23:32:39 +0300

    wip on Chunk: pod implementation

--- lib/public/memory/object_pool.h
+++ lib/public/memory/object_pool.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <memory>
 
 /*
@@ -36,18 +37,85 @@ namespace memory
      	* consisting of an integral number of fixed-size blocks. Chunk contains
      	* logic that allows to allocate and deallocate memory blocks.
      	* 
+     	* Chunk is made blockSize agnostic.
+     	* 
      	* When there are no more blocks available in the chunk, the allocation
      	* function fails by returning zero.
+     	* 
+     	* The number of blocks is limited to a value that fits in an 
+     	* unsigned char.
      	*/
     	struct Chunk {
-	    	void init(std::size_t blockSize,
-	    			  unsigned char blocks);
-	    	void* alloc(std::size_t blockSize);
-	    	void dealloc(void* p, std::size blockSize);
+    		// Initialize a chunk object.
+    		void init(std::size_t blockSize, unsigned char blocks) {	    		
+    			assert(blockSize > 0);
+	    		data_ = new unsigned char[blockSize * blocks];
+	    		firstAvailableBlock_ = 0;
+	    		blocksAvailable_ = blocks;
+	    		// Set the first byte of an unused block to hold the index of
+	    		// the next unused block (single linked list melted inside the
+	    		// data).
+	    		unsigned char* p = data_;
+	    		for (unsigned char i = 0; i != blocks; p += blockSize) {
+	    			*p = ++i;
+	    		}
+	    	} // init
+
+	    	// Release the allocated memory of a chunk.
+	    	void release() {
+	    		blocksAvailable_ = 0;
+	    		delete[](data_);
+	    	} // release
+
+	    	// Allocate memory for a block inside chunk.
+	    	// (Time complexity - linear)
+	    	void* alloc(std::size_t blockSize) {
+	    		assert(blockSize > 0;)
+	    		// Implementation idea: fetch the block indexed 
+	    		// by firstAvailableBlock_ and adjust firstAvailableBlock_ 
+	    		// to refer to the next available block.
+	    		if (!blocksAvailable_) {
+	    			return 0;
+	    		}
+	    		unsigned char* result = 
+	    			data_ + (firstAvailableBlock_ * blockSize);
+	    		firstAvailableBlock_ = *result;
+	    		--blocksAvailable;
+	    		return result;
+	    	} // alloc
+
+	    	// Release memory of a block insied chunk.
+	    	// If the wrong pointer is passed - behaviour is undefined.
+	    	// (Time complexity - linear).
+	    	void dealloc(void* block, std::size blockSize) {
+	    		assert(blockSize > 0);
+	    		assert(block >= data_);
+
+	    		unsigned char* toRelease = static_cast<unsigned char*>(block);
+
+	    		// Alignment check.
+	    		assert(((toRelease - data_) % blockSize) == 0);
+
+	    		// toRelease points to a block now which is considered 
+	    		// available, so the first byte of the block pointed by
+	    		// toRelease must hold indexed of next available  block,
+	    		// which is still firstAvailableBlock_.
+	    		*toRelease = firstAvailableBlock_;
+
+	    		firstAvailableBlock_ = 
+	    			static_cast<unsigned char>(toRelease - data_) / blockSize;
+
+	    		// Truncation check.
+	    		assert(firstAvailableBlock_ ==
+	    			((toRelease - data_) / blockSize));
+
+	    		++blocksAvailable_;
+	    	} // dealloc
+
 	    	unsigned char* data_;
 	    	unsigned char firstAvailableBlock_,
 	    		blocksAvailable_;
-    	};
+    	}; // struct Chunk
     };
 
 

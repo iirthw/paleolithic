a05993c74ebedf5951f0e897d97e0b3c35b40c94
iirthw <iirthw@gmail.com>
2021-09-02 22:20:21 +0300

    wip on SmallObject

--- lib/public/memory/object_pool.h
+++ lib/public/memory/object_pool.h
@@ -187,4 +187,10 @@ namespace memory
      * c++ classes. Client objects benefit from specialized allocation by
      * simply deriving from SmallObject.
      */
+    class SmallObject {
+    public:
+    	static void* operator new(std::size_t size);
+    	static void* operator delete(void* p, std::size_t size);
+    	virtual ~SmallObject() {}
+    }; // class SmallObject
 } // namespace memory

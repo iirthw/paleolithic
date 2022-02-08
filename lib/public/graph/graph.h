6eebdb997b6422a3ec196e584afa7b96324d4b32
iirthw <iirthw@gmail.com>
2022-02-08 23:21:51 +0200

    add graph.h

---
+++ lib/public/graph/graph.h
@@ -0,0 +1,14 @@
+namespace graph
+{
+    class AdjacencyMatrix
+    {
+
+    }; // class AdjacencyMatrix
+
+
+    template <typename T>
+    class Graph
+    {
+        Graph(size_t nodeCount);
+    }; // class Graph
+} // namespace paleo

3e486213292d25838990ef3efd0ba9323e87372c
iirthw <iirthw@gmail.com>
2022-02-09 23:49:42 +0200

    wip on graph

--- lib/public/graph/graph.h
+++ lib/public/graph/graph.h
@@ -1,14 +1,23 @@
+#include <utility>
+
 namespace graph
 {
+    using Edge = std::pair<int, int>;
+
     class AdjacencyMatrix
     {
+    public:
+        AdjacencyMatrix(size_t nodeCount);
 
+        void addEdge(const Edge& edge);
     }; // class AdjacencyMatrix
 
 
     template <typename T>
     class Graph
     {
+    public:
         Graph(size_t nodeCount);
+        Graph(const AdjacencyMatrix& adjMatrix);
     }; // class Graph
 } // namespace paleo

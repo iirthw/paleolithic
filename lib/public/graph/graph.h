2cd40f29e5d3fab1c0a44c61a5bae2abaf0b66dd
iirthw <iirthw@gmail.com>
2022-02-10 23:56:52 +0200

    wip on graph (2)

--- lib/public/graph/graph.h
+++ lib/public/graph/graph.h
@@ -2,22 +2,43 @@
 
 namespace graph
 {
-    using Edge = std::pair<int, int>;
+    template <typename Index, typename Data>
+    class Vertex
+    {
+    public:
+        Vertex(Index id, const Data& data)
+        : m_id(id)
+        , m_data(data)
+        {}
 
+        Index id() const { return m_id; }
+        Data data() const { return m_data; }
+
+    private:
+        Index m_id;
+        Data m_data;
+    }; // class Vertex
+
+    template <typename Index>
+    using Edge = std::pair<Index, Index>;
+
+    template <typename Index, Directed>
     class AdjacencyMatrix
     {
     public:
         AdjacencyMatrix(size_t nodeCount);
 
-        void addEdge(const Edge& edge);
+        void addEdge(const Edge<Index>& edge);
+        bool exists(const Edge<Index>& edge) const;
+        bool exists(Index a, Index b) const;
     }; // class AdjacencyMatrix
 
 
-    template <typename T>
+    template <typename Vertex, Index, Directed>
     class Graph
     {
     public:
         Graph(size_t nodeCount);
-        Graph(const AdjacencyMatrix& adjMatrix);
+        Graph(const AdjacencyMatrix<Index, Directed>& adjMatrix);
     }; // class Graph
 } // namespace paleo

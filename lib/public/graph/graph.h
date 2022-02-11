8b51ab0dfe19c7023a625f132511871cf8639574
iirthw <iirthw@gmail.com>
2022-02-11 22:39:55 +0200

    wip on graph (3)

--- lib/public/graph/graph.h
+++ lib/public/graph/graph.h
@@ -2,43 +2,49 @@
 
 namespace graph
 {
-    template <typename Index, typename Data>
+    template <typename VertexIndex, typename Data>
     class Vertex
     {
     public:
-        Vertex(Index id, const Data& data)
+        Vertex(VertexIndex id, const Data& data)
         : m_id(id)
         , m_data(data)
         {}
 
-        Index id() const { return m_id; }
+        VertexIndex id() const { return m_id; }
         Data data() const { return m_data; }
 
     private:
-        Index m_id;
+        VertexIndex m_id;
         Data m_data;
     }; // class Vertex
 
-    template <typename Index>
-    using Edge = std::pair<Index, Index>;
+    template <typename VertexIndex, typename EdgeAttribute>
+    struct Edge
+    {
+        VertexIndex begin;
+        VertexIndex end;
+        EdgeAttribute attrib;
+    }; // struct Edge
 
-    template <typename Index, Directed>
+    template <typename VertexIndex, typename EdgeAttribute, typename Directed>
     class AdjacencyMatrix
     {
     public:
         AdjacencyMatrix(size_t nodeCount);
 
-        void addEdge(const Edge<Index>& edge);
-        bool exists(const Edge<Index>& edge) const;
-        bool exists(Index a, Index b) const;
+        void addEdge(const Edge<VertexIndex, EdgeAttribute>& edge);
+        bool exists(const Edge<VertexIndex, EdgeAttribute>& edge) const;
+        bool exists(VertexIndex a, VertexIndex b) const;
     }; // class AdjacencyMatrix
 
 
-    template <typename Vertex, Index, Directed>
+    template <typename Vertex, typename VertexIndex,
+        typename EdgeAttribute,  typename Directed>
     class Graph
     {
     public:
         Graph(size_t nodeCount);
-        Graph(const AdjacencyMatrix<Index, Directed>& adjMatrix);
+        Graph(const AdjacencyMatrix<VertexIndex, EdgeAttribute, Directed>& adjMatrix);
     }; // class Graph
 } // namespace paleo
